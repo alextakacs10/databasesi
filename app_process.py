31f16be247f202da4997df761afde06190d50f66
Alex Takács <alextakacs10@gmail.com>
2017-05-21 23:53:08 +0200

    Divided logics into function, main function is more readable this way.

--- app_process.py
+++ app_process.py
@@ -28,35 +28,45 @@ def reset(filename):
         sys.exit()
 
 
-
-def main():
-    status = input("Are you working right now? [y/n]")
+def get_user_status():
+    status = input("Are you working right now? [y/n]")  
     if status == "y":
         at_work = True
     else:
         at_work = False
-        print("OK, goodbye then!")
+    return at_work
+
+
+def show_menu():
+    print("\nWelcome! The HR department wants you to do the following tasks today: \n")
+    tasks = ["Get the first name and the last name of all the mentors",
+             "Get the nickname of every mentor working at Miskolc",
+             "Get the full name and phone number of Carol, who may have left her hat at us.",
+             "If it wasn't Carol, there might be another girl. Pleace get her full name and phone number, all they know is her email address is ending in @adipiscingenimmi.edu.",
+             "A new applicant appeared. Please add him to the database with the following info: Markus Schaffarzyk, 003620/725-2666, djnovus@groovecoverage.com, 54823. After that, please print his info.",
+             "Jemima Foreman changed her phone number to 003670/223-7459, please update it in the database.",
+             "Arsenio and his friend from mauriseu.net canceled the application process. Please delete them from our database. You can find them by their email address ending in @mauriseu.net."]
+    for num, task in enumerate(tasks):
+        print("{}. {}".format(num+1, task))
+    print("\nIf you are done, you can go home. Just press [q] to quit.")
+    print("To reset the database with sample data press [r].")
+    print("\n----------------------------")
+
+
+def main():
+    at_work = get_user_status()
+    if not at_work:
+        print("Okay, good bye then!")
 
     while at_work:
-        print("\nWelcome! The HR department wants you to do the following tasks today: \n")
-        tasks = ["1. Get the first name and the last name of all the mentors",
-                "2. Get the nickname of every mentor working at Miskolc",
-                "3. Get the full name and phone number of Carol, who may have left her hat at us.",
-                "4. If it wasn't Carol, there might be another girl. Pleace get her full name and phone number, all they know is her email address is ending in @adipiscingenimmi.edu.",
-                "5. A new applicant appeared. Please add him to the database with the following info: Markus Schaffarzyk, 003620/725-2666, djnovus@groovecoverage.com, 54823. After that, please print his info.",
-                "6. Jemima Foreman changed her phone number to 003670/223-7459, please update it in the database.",
-                "7. Arsenio and his friend from mauriseu.net canceled the application process. Please delete them from our database. You can find them by their email address ending in @mauriseu.net."]
-        for task in tasks:
-            print(task)
-        print("\nIf you are done, you can go home. Just press [q] to quit.")
-        print("To reset the database with sample data press [r].")
-        print("\n----------------------------")
+        show_menu()
         action = input("What would you like to do? ")
 
         if action == "r":
             reset("application_process_sample_data.sql")
         elif action == "q":
             sys.exit("Goodbye!")
+        elif action == ""
 
 
 if __name__ == '__main__':
