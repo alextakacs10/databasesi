bb6c8ba965ddfd59cdbebe712b84fdab15e2178d
Alex Takács <alextakacs10@gmail.com>
2017-06-06 01:24:48 +0200

    Renamed query vessel variable to a more general form.

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import psycopg2
 import db_login
 
 
-def handle_data(SQL):
+def handle_data(query):
     """Initiate connection -> retrieve data -> close connection"""
 
     try:
@@ -10,10 +10,10 @@ def handle_data(SQL):
         login_info = "dbname='{}' user='{}' host='{}' password='{}'".format(db, user, host, pw)
         conn = psycopg2.connect(login_info)
         cursor = conn.cursor()
-        cursor.execute(SQL)
+        cursor.execute(query)
         header = [desc[0] for desc in cursor.description]
         result = cursor.fetchall()
-        return result, header
+        return header, result
     except psycopg2.DatabaseError:
         print("Can't connect to the server, try again.")
         abort(505)
